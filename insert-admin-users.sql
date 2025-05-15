ce5bd788dcbcdeff03d67a11a85bf0939d83a90d
Ângelo Azevedo <ang3lo@azevedos.eu.org>
2025-05-15 20:38:38 +0100

    Add insert-admin-users.sql to Dockerfile and remove insert-test-data.sql

---
+++ insert-admin-users.sql
@@ -0,0 +1,6 @@
+-- Insert admin users
+INSERT INTO rl.user (role, name, email, created_at)
+VALUES
+    ('A', 'Ângelo Azevedo', 'a50565@alunos.isel.pt', now()),
+    ('A', 'António Alves', 'a50536@alunos.isel.pt' , now()),
+    ('A', 'Pedro Matutino', 'pedro.miguens@isel.pt' , now());
