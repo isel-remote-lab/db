ba958ca1cde55412e245466a5d39702c8994ad2f
Antonio Alves <a50539@alunos.isel.pt>
2025-06-11 18:07:43 +0100

    Update email address for António Alves in insert-admin-users.sql

--- insert-admin-users.sql
+++ insert-admin-users.sql
@@ -2,5 +2,5 @@
 INSERT INTO rl.user (role, name, email, created_at)
 VALUES
     ('A', 'Ângelo Azevedo', 'a50565@alunos.isel.pt', now()),
-    ('A', 'António Alves', 'a50536@alunos.isel.pt' , now()),
+    ('A', 'António Alves', 'a50539@alunos.isel.pt' , now()),
     ('A', 'Pedro Matutino', 'pedro.miguens@isel.pt' , now());
