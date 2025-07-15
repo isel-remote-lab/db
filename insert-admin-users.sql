0ba6cc482828229f81ab5db0dcf181808affb376
Ângelo Azevedo <ang3lo@azevedos.eu.org>
2025-07-15 12:49:28 +0100

    Add new admin users to insert-admin-users.sql

--- insert-admin-users.sql
+++ insert-admin-users.sql
@@ -3,7 +3,9 @@ INSERT INTO rl.user (role, name, email, created_at)
 VALUES
     ('A', 'Ângelo Azevedo', 'a50565@alunos.isel.pt', now()),
     ('A', 'António Alves', 'a50539@alunos.isel.pt' , now()),
-    ('A', 'Pedro Matutino', 'pedro.miguens@isel.pt' , now());
+    ('A', 'Pedro Matutino', 'pedro.miguens@isel.pt' , now()),
+    ('A', 'Diego Passos', 'diego.passos@isel.pt' , now()),
+    ('A', 'Fernando Sousa', 'fernando.sousa@isel.pt' , now());
 
 -- Insert hardware
 INSERT INTO rl.hardware (name, serial_number, status, mac_address, ip_address, created_at)
